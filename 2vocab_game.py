05b9624f77a4630d4023102ce1e883d913c19671
413-15-natchaya <54416@yupparaj.ac.th>
2026-08-26 12:55:10 +0700

    Update and rename vocab_game to 2vocab_game.py

--- 2vocab_game.py
+++ 2vocab_game.py
@@ -101,5 +101,5 @@ if st.session_state.get("is_ended", False):
     show_result_dialog(ans1, ans2)
 
 st.divider()
-st.write("นางสาวดีใจ ยิ้มแย้ม เลขที่ 5 ม.4/5")
+st.write("นางสาวณัฐชยา เป็งธรรม เลขที่ 15 ม.4/13")
 
